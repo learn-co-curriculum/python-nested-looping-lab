5969da0a537c0de8c6f64cf19e3e983330203898
tkoar <terrancekoar@gmail.com>
2018-09-18 09:36:18 -0400

    merging in test changes from master branch

--- pytests/test_index.py
+++ pytests/test_index.py
@@ -2,44 +2,52 @@ import pytests
 from ipynb.fs.full.index import countries, colors, players, captains, home_team_players, forwards, player_with_highest_num, player_names
 
 def test_countries_list():
+    assert countries is not None
     assert type(countries) == type([])
     assert len(countries) == 2
     assert countries == ['France', 'Australia']
 
 def test_colors_list():
+    assert colors is not None
     assert type(colors) == type([])
     assert len(colors) == 5
     assert colors == ['blue', 'white', 'red', 'green', 'gold']
 
 def test_players_list():
+    assert players is not None
     assert type(players) == type([])
     assert type(players[0]) == type({})
     assert len(players) == 22
     assert players == return_players()
 
 def test_captains_list():
+    assert captains is not None
     assert type(captains) == type([])
     assert type(captains[0]) == type({})
     assert len(captains) == 2
     assert captains == [{'name': 'Hugo LLORIS', 'captain': True, 'shirt_number': 1, 'position': 'Goalie'}, {'name': 'Mile JEDINAK', 'captain': True, 'shirt_number': 15, 'position': 'Midfield'}]
 
 def test_home_team_players_list():
+    assert home_team_players is not None
     assert type(home_team_players) == type([])
     assert type(home_team_players[0]) == type({})
     assert len(home_team_players) == 11
     assert home_team_players == return_home_team_players()
 
 def test_forwards_list():
+    assert forwards is not None
     assert type(forwards) == type([])
     assert type(forwards[0]) == type({})
     assert len(forwards) == 3
     assert forwards == [{'name': 'Mathew LECKIE', 'captain': False, 'shirt_number': 7, 'position': 'Forward'}, {'name': 'Robbie KRUSE', 'captain': False, 'shirt_number': 10, 'position': 'Forward'}, {'name': 'Andrew NABBOUT', 'captain': False, 'shirt_number': 11, 'position': 'Forward'}]
 
 def test_player_with_highest_num_dict():
+    assert player_with_highest_num is not None
     assert type(player_with_highest_num) == type({})
     assert player_with_highest_num == {'name': 'Tom ROGIC', 'captain': False, 'shirt_number': 23, 'position': 'Midfield'}
 
 def test_player_names_list():
+    assert player_names is not None
     assert type(player_names) == type([])
     assert type(player_names[0]) == type("")
     assert len(player_names) == 22
